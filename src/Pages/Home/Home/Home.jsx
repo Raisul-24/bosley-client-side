3f9a417eadc89bc20822160ecc07ea4f8962fdd7
Raisul Islam Hridoy <raisul15-3813@diu.edu.bd>
2024-02-01 22:51:18 +0600

    [Home]:added seeninand learn more section

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,16 @@
 
 import Banner from "../Banner/Banner";
+import LearnMore from "../LearnMore/LearnMore";
+import SeenIn from "../SeenIn/SeenIn";
 
 const Home = () => {
   return (
     <>
       <Banner />
-      
+
+
+      <SeenIn></SeenIn>
+      <LearnMore></LearnMore>
     </>
   );
 };
