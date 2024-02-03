024b6e7b0a6fefe5ff2a12277aaff372e1512ab4
Raisul Islam Hridoy <raisul15-3813@diu.edu.bd>
2024-02-03 23:58:50 +0600

    [Home]: add slider

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 
+import { Slider } from "@material-tailwind/react";
 import Banner from "../Banner/Banner";
 import HairRestorationDetailsSection from "../HairRestorationDetailsSection/HairRestorationDetailsSection";
 import HairTransplantSolution from "../HairTransplantSolution/HairTransplantSolution";
 import LearnMore from "../LearnMore/LearnMore";
 import NonSurgicalRegrowthSection from "../NonSurgicalRegrowthSection/NonSurgicalRegrowthSection";
 import SeenIn from "../SeenIn/SeenIn";
+import SliderBanner from "../SliderBanner/SliderBanner";
 
 const Home = () => {
   return (
@@ -13,6 +15,7 @@ const Home = () => {
       <HairTransplantSolution />
       <NonSurgicalRegrowthSection />
       <HairRestorationDetailsSection />
+      <SliderBanner></SliderBanner>
       <SeenIn></SeenIn>
       <LearnMore></LearnMore>
     </>
---
+++ src/Pages/Home/SliderBanner/SliderBanner.jsx
@@ -0,0 +1,11 @@
+
+
+const SliderBanner = () => {
+   return (
+      <div className="mb-10">
+         <h2 className="text-xl text-center font-semibold">Join the millions of men and women who have come to <br /> Bosley for answers to their hair restoration questions</h2>
+      </div>
+   );
+};
+
+export default SliderBanner;
